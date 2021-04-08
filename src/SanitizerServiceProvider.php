fc46c37be794677c35641b04269186303a7e5ea9
Peeyush Budhia <peeyush.budhia@gmail.com>
2021-04-08 11:22:47 +0530

    Publishing

--- src/SanitizerServiceProvider.php
+++ src/SanitizerServiceProvider.php
@@ -11,17 +11,25 @@ class SanitizerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
+        /*
+         * Optional methods to load your package assets
+         */
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'sanitizer');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'sanitizer');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-            # Publishing tests.
-            // $this->publishes([
-            //     __DIR__.'/../tests' => base_path('tests/Unit'),
-            // ], 'views');
-
+        if ($this->app->runningInConsole()) 
+        {
             # Publish stubs
             $this->publishes([
-                __DIR__.'/../stubs/request.stub' => base_path('stubs'),
+                __DIR__.'/../stubs' => base_path('stubs'),
             ], 'stubs');
+
+            # Publish Unit tests
+            $this->publishes([
+                __DIR__.'/../tests/Unit' => base_path('tests/Unit'),
+            ], 'tests');
         }
     }
 
@@ -31,7 +39,7 @@ class SanitizerServiceProvider extends ServiceProvider
     public function register()
     {
         // Register the main class to use with the facade
-        $this->app->singleton('sanitizer', function ($app) {
+        $this->app->singleton('sanitizer', function () {
             return new Factory;
         });
     }
